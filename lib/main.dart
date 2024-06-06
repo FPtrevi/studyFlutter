0f17f219da92a66dac537df574fe0b04a557442e
chan <dhguscks20@naver.com>
2024-06-06 15:16:33 +0900

    UI challenge Header

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 
+class Player {
+  String? name;
+  // 1
+  //Player(this.name)
+  Player({required this.name});
+}
+
 void main() {
+  // 1
+  // var nico = Player("nico");
+  var nico = Player(name: "potato");
   runApp(App());
 }
 
@@ -8,16 +18,42 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          elevation: 10,
-          title: Text('Hello flutter!'),
-        ),
-        body: Center(
-          child: Text('Hello world'),
-        ),
-      ),
-    );
+        home: Scaffold(
+      backgroundColor: Color(0xFF181818),
+      body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Hey, Selena',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.8),
+                          fontSize: 18,
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              )
+            ],
+          )),
+    ));
   }
 }
